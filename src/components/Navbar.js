643269d3f25947fff152f86cd2fd0a40dc656e32
hema gopisetty <hemagopisetty2@gmail.com>
2021-09-17 19:41:22 +0530

    ShoopingCart

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+  
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+ const Navbar = ()=>{
+    return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link to="/" className="brand-logo">QUEENS-ERA</Link>
+                    
+                    <ul className="right">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Shop</Link></li>
+                        
+              <li><Link to="/cart">My cart</Link></li>
+                        <li><Link to="/cart"><i className="material-icons">add_shopping_cart</i></Link></li>
+                    </ul>
+                </div>
+            </nav>
+   
+        
+    )
+}
+
+export default Navbar;
